38ca4c4b56f8bb5fc87a7a0df8b663d38b1c8a79
Ben Grinberg <bengrin25@gmail.com>
2015-12-30 18:25:06 -0500

    initial commit

---
+++ client/app/links/links.js
@@ -0,0 +1,9 @@
+angular.module('shortly.links', [])
+
+.controller('LinksController', function ($scope, Links) {
+  angular.extend($scope, Links);
+  $scope.data = {
+    links: [{}, {}, {}]
+  };
+  $scope.getLinks();
+});
